aaed04b9cef16bd3e4448a9b5c0c1706b56a4b70
DasAlvarp <alvaroguis@gmail.com>
2015-11-14 16:16:13 -0600

    did more stuff!  :white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square::white_medium_square:

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import HttpResponse
 
-# Create your views here.
+from .models import Question
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    output = ', '.join([p.question_text for p in latest_question_list])
+
+def index(request):
+    return HttpResponse("OMG YOU CAN SEE A THING!")
